bbf0294d6be270634e8966152158cd297160f589
Deathblood69 <deathblood69002@gmail.com>
2024-03-22 12:43:03 +0100

    feat(orbat): Orbat - Armée NATO

--- orbat/nato/nato_army.hpp
+++ orbat/nato/nato_army.hpp
@@ -30,6 +30,50 @@
 // == SECTIONS ===================================//
 //================================================//
 
+    class NATO_S1 : SectionInfanterie {
+        id=1;
+        side= "West";
+        commander="Miller";
+        text="Falcon"
+        textShort="Falcon"
+        description="Les derniers survivants de la FO Aegis, dirigé par le capitaine Scott Miller (UKSF)"
+    	subordinates[]= {"NATO_S1_E1", "NATO_S1_E2", "NATO_S1_E3", "NATO_S1_E4"};
+    };
+
 //================================================//
 // == ESCOUADES ==================================//
 //================================================//
+
+    class NATO_EscouadeInfanterie : EscouadeInfanterie {
+        idType=2
+        side= "West";
+        text="%3 %1"
+        textShort="%3 %1"
+
+    }
+
+    class NATO_S1_E1 : NATO_EscouadeInfanterie {
+        id=1;
+        commander="Conway";
+    	assets[]= {};
+    };
+
+
+    class NATO_S1_E2 : NATO_EscouadeInfanterie {
+        id=2;
+        commander="Lacey";
+    	assets[]= {};
+    };
+
+    class NATO_S1_E3 : NATO_EscouadeInfanterie {
+        id=3;
+        commander="Mitchell";
+        assets[]= {};
+    };
+
+    class NATO_S1_E4 : NATO_EscouadeInfanterie {
+        id=4;
+        commander="James";
+        assets[]= {};
+    };
+
